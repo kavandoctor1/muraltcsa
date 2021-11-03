5d5a6f665d0169390b208d87a225ef42ae460705
kavandoctor1 <kavandoctor1@gmail.com>
2021-11-02 21:38:19 -0700

    Added registration feature

--- app.py
+++ app.py
@@ -4,20 +4,23 @@ import flask
 import flask_login
 import os
 app = Flask(__name__)
-
 login_manager = flask_login.LoginManager()
 app.secret_key = os.urandom(24)
 login_manager.init_app(app)
-users = {'kavandoctor': {'password': 'kavandoctor1'}}
-
+def load_users():
+    USERS = eval(open('users.txt','r').read())
+    return USERS
+def save_users(USERS):
+    open('users.txt','w').write(str(USERS))
 class User(flask_login.UserMixin):
     pass
 @login_manager.user_loader
-def user_loader(email):
-    if email not in users:
+def user_loader(username):
+    USERS = load_users()
+    if username not in USERS:
         return
     user = User()
-    user.id = email
+    user.id = username
     return user
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,14 +28,37 @@ def login():
     if flask.request.method == 'GET':
         return render_template('login.html',incorrect = False)
 
-    email = flask.request.form['email']
-    if email in users and flask.request.form['password'] == users[email]['password']:
+    username = flask.request.form['username']
+    USERS = load_users()
+    if username in USERS and flask.request.form['password'] == USERS[username]['password']:
+        load_users()
         user = User()
-        user.id = email
+        user.id = username
         flask_login.login_user(user)
         return flask.redirect('/find')
     else:
         return render_template('login.html',incorrect=True)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if flask.request.method == 'GET':
+        return render_template('register.html',incorrect = False,exists=False)
+
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    cbusername = flask.request.form['cbusername']
+    cbpassword = flask.request.form['cbpassword']
+    print(cbusername,cbpassword)
+    USERS = load_users()
+    if username in USERS:
+        return render_template('register.html',incorrect=False,exists=True)
+    if not isvalid(cbusername,cbpassword):
+        return render_template('register.html',incorrect=True,exists=False)
+    load_users()
+    USERS[username] = {"password":password,"cbusername":cbusername,"cbpassword":cbpassword}
+    save_users(USERS)
+    return flask.redirect('/login')
+    
 
 @app.route('/logout')
 def logout():
@@ -45,11 +71,12 @@ def unauthorized_handler():
 
 @login_manager.request_loader
 def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
+    username = request.form.get('username')
+    USERS = load_users()
+    if username not in USERS:
         return
     user = User()
-    user.id = email
+    user.id = username
     return user
 
 
@@ -61,16 +88,18 @@ import twill.commands as tw
 
 @app.route('/find',methods=['GET'])
 @flask_login.login_required
-def contact():
+def exercises():
     print(request.data,file=sys.stderr)
+    USERS = load_users()
     username = flask_login.current_user.id
-    password = users[username]['password']
+    cbusername = USERS[username]['cbusername']
+    cbpassword = USERS[username]['cbpassword']
     # tw.follow('https://practiceit.cs.washington.edu/user/problems-solved')
 
     tw.go('http://practiceit.cs.washington.edu/login')
 
-    tw.fv("1", "usernameoremail", username)
-    tw.fv("1", "password", password)
+    tw.fv("1", "usernameoremail", cbusername)
+    tw.fv("1", "password", cbpassword)
     tw.submit('login')
     tw.go('https://practiceit.cs.washington.edu/user/problems-solved')
 
@@ -90,7 +119,17 @@ def contact():
     print(send)
     tw.reset_browser()
     return render_template('exercises.html',send=send)
+def isvalid(username,password):
+    tw.reset_browser()
+    tw.go('http://practiceit.cs.washington.edu/login')
 
+    tw.fv("1", "usernameoremail", username)
+    tw.fv("1", "password", password)
+    tw.submit('login')
+    tw.go('https://practiceit.cs.washington.edu/')
+    s = tw.show()
+    tw.reset_browser()
+    return 'Start' in s
 def get_calendar():
     l = open('calendar.txt','r').read().strip().split('\n\n')
     calendar = []
@@ -106,3 +145,4 @@ def get_calendar():
         calendar.append([date,ex])
     return calendar
 # print(get_calendar())
+
